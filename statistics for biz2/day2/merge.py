8414c085a1fa84c6db108bb363a140312d36de78
dmlin7777777 <dml077@outlook.com>
2025-08-19 16:25:20 +0800

    Update merge.py

--- statistics for biz2/day2/merge.py
+++ statistics for biz2/day2/merge.py
@@ -1,8 +1,8 @@
 import pandas as pd
 pd.options.mode.copy_on_write = True
-file1 = r'C:\Users\12932\Desktop\nus\statistics for biz2\Day 2\Table1_Sales_Order.xlsx'
-file2 = r'C:\Users\12932\Desktop\nus\statistics for biz2\Day 2\Table2_Region.xlsx'
-file3 = r'C:\Users\12932\Desktop\nus\statistics for biz2\Day 2\Table3_Product.xlsx'
+file1 = r'Table1_Sales_Order.xlsx'
+file2 = r'Table2_Region.xlsx'
+file3 = r'Table3_Product.xlsx'
 
 df1 = pd.read_excel(file1,engine="openpyxl")
 df2 = pd.read_excel(file2,engine="openpyxl")
@@ -18,10 +18,10 @@ result2 = pd.merge(
     left = result1,
     right = df3,
     how = 'left',
-    on = 'Product ID'
-    )
+    on = 'Product ID')
 
 result2['TOTAL'] = result2['Units'] * result2['Price']
 
 
-result2.to_excel(r'C:\Users\12932\Desktop\nus\statistics for biz2\Day 2\merge.xlsx',index = False)
+result2.to_excel(r'merge.xlsx',index = False)
+
