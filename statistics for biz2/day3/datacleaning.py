0393db18f19cdfa813f622c088784a93a3212b62
dmlin7777777 <dml077@outlook.com>
2025-08-21 12:40:52 +0800

    Update datacleaning.py

--- statistics for biz2/day3/datacleaning.py
+++ statistics for biz2/day3/datacleaning.py
@@ -6,14 +6,8 @@ file1 = r'titanic.csv'
 
 df = pd.read_csv(file1)
 df = df[['PassengerId','Survived',	'Pclass','Sex',	'Age']]
-# def gender1(gender):
-#     if gender == 'female':
-#         gendernew = 0
-#     else:
-#         gendernew = 1
-#     return gendernew
-
 df['Age'] = round(df.groupby('Pclass')['Age'].transform(
     lambda x: x.fillna(x.mean())
 
 df.to_csv(r'titanic_cleaned.csv',index = False)
+
